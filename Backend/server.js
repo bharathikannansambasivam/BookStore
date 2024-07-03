d88e83821fc6633446aee8a106903ee53722f11b
bharathikannansambasivam <bharathikannansambasivam@gmail.com>
2024-07-03 19:39:03 +0530

    .env added

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
 const { PORT, MONGOURL } = require("./config");
 const BookRoute = require("./routes/route");
 const BookModel = require("./models/bookmodel");
+const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI;
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 const { default: mongoose } = require("mongoose");
-
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 app.use(express.json());
 app.use("/books", BookRoute);
 mongoose
-  .connect(MONGOURL)
+  .connect(mongoUri)
   .then(() => {
     console.log("APP connected TO Database");
-    app.listen(PORT, () => {
-      console.log(`PORT RUNNING ON ${PORT}`);
+    app.listen(port, () => {
+      console.log(`PORT RUNNING ON ${port}`);
     });
   })
   .catch((e) => {
