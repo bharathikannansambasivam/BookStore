921138f753b3b0c6f485300813182b28a2cd7d59
bharathikannansambasivam <91874993+bharathikannansambasivam@users.noreply.github.com>
2024-07-05 11:24:28 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,3 @@
 const PORT = 5555;
-const MONGOURL = "mongodb+srv://bharathi:bharathi@cluster0.mhlsybo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGOURL = "mongodb+srv://bharathi:bharathi@cluster0bookstore.j3sgiff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0Bookstore";
 module.exports = { PORT, MONGOURL };
